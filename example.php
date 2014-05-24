ee4d6aa7deb4caff7fa6d7b00343fece09c5285b
zhushunqing.iMac <zhushunqing@gmail.com>
2014-05-24 22:47:02 +0800

    update

--- example.php
+++ example.php
@@ -1,8 +1,25 @@
 <?php
 
 	require_once('GridPHP.inc.php');
-	
+
 	$GP = &$GLOBALS['GRIDPHP'];
+
+	//def下默认加载直接使用
+	
+	// $memc = $GP->memcd->loadMemc('server');
+	// $memc->set('xxx', 1);
+	// $memc->get('xxx');
+
+	// $mdb = $this->loadDB('user');
+	// $row = array(
+	// 	'user_id'	=> 12345,
+	// 	'name'		=> 'jack',
+	// );
+	// $mdb->insert('user', $row);
+	// $mdb->close();
+
+
+	//mod下调用前加载
 	$GP->mod('modexample');
 
 	$rs = array();
